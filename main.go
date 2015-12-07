5b73d5a652ea3285b97acb518dae7cb82858fa8c
pankona <yosuke.akatsuka@gmail.com>
2015-12-07 22:28:29 +0900

    initial commit. Gopher doesn't start to rotate yet.

---
+++ main.go
@@ -0,0 +1,164 @@
+// Copyright 2014 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// +build darwin linux
+
+// An app that demonstrates the sprite package.
+//
+// Note: This demo is an early preview of Go 1.5. In order to build this
+// program as an Android APK using the gomobile tool.
+//
+// See http://godoc.org/golang.org/x/mobile/cmd/gomobile to install gomobile.
+//
+// Get the sprite example and use gomobile to build or install it on your device.
+//
+//   $ go get -d golang.org/x/mobile/example/sprite
+//   $ gomobile build golang.org/x/mobile/example/sprite # will build an APK
+//
+//   # plug your Android device to your computer or start an Android emulator.
+//   # if you have adb installed on your machine, use gomobile install to
+//   # build and deploy the APK to an Android target.
+//   $ gomobile install golang.org/x/mobile/example/sprite
+//
+// Switch to your device or emulator to start the Basic application from
+// the launcher.
+// You can also run the application on your desktop by running the command
+// below. (Note: It currently doesn't work on Windows.)
+//   $ go install golang.org/x/mobile/example/sprite && sprite
+package main
+
+import (
+	"image"
+	"log"
+	"time"
+
+	_ "image/jpeg"
+
+	"golang.org/x/mobile/app"
+	"golang.org/x/mobile/asset"
+	"golang.org/x/mobile/event/lifecycle"
+	"golang.org/x/mobile/event/paint"
+	"golang.org/x/mobile/event/size"
+	"golang.org/x/mobile/exp/app/debug"
+	"golang.org/x/mobile/exp/f32"
+	"golang.org/x/mobile/exp/gl/glutil"
+	"golang.org/x/mobile/exp/sprite"
+	"golang.org/x/mobile/exp/sprite/clock"
+	"golang.org/x/mobile/exp/sprite/glsprite"
+	"golang.org/x/mobile/gl"
+)
+
+var (
+	startTime = time.Now()
+	images    *glutil.Images
+	eng       sprite.Engine
+	scene     *sprite.Node
+	fps       *debug.FPS
+)
+
+func main() {
+	app.Main(func(a app.App) {
+		var glctx gl.Context
+		var sz size.Event
+		for e := range a.Events() {
+			switch e := a.Filter(e).(type) {
+			case lifecycle.Event:
+				switch e.Crosses(lifecycle.StageVisible) {
+				case lifecycle.CrossOn:
+					glctx, _ = e.DrawContext.(gl.Context)
+					onStart(glctx)
+					a.Send(paint.Event{})
+				case lifecycle.CrossOff:
+					onStop()
+					glctx = nil
+				}
+			case size.Event:
+				sz = e
+			case paint.Event:
+				if glctx == nil || e.External {
+					continue
+				}
+				onPaint(glctx, sz)
+				a.Publish()
+				a.Send(paint.Event{}) // keep animating
+			}
+		}
+	})
+}
+
+func onStart(glctx gl.Context) {
+	images = glutil.NewImages(glctx)
+	fps = debug.NewFPS(images)
+	eng = glsprite.Engine(images)
+	loadScene()
+}
+
+func onStop() {
+	eng.Release()
+	fps.Release()
+	images.Release()
+}
+
+func onPaint(glctx gl.Context, sz size.Event) {
+	glctx.ClearColor(1, 1, 1, 1)
+	glctx.Clear(gl.COLOR_BUFFER_BIT)
+	now := clock.Time(time.Since(startTime) * 60 / time.Second)
+	eng.Render(scene, now, sz)
+	fps.Draw(sz)
+}
+
+func newNode() *sprite.Node {
+	n := &sprite.Node{}
+	eng.Register(n)
+	scene.AppendChild(n)
+	return n
+}
+
+func loadScene() {
+	texs := loadTextures()
+	scene = &sprite.Node{}
+	eng.Register(scene)
+	eng.SetTransform(scene, f32.Affine{
+		{1, 0, 0},
+		{0, 1, 0},
+	})
+
+	var n *sprite.Node
+
+	n = newNode()
+	eng.SetSubTex(n, texs[texGopherR])
+	eng.SetTransform(n, f32.Affine{
+		{140, 0, 100},
+		{0, 90, 100},
+	})
+}
+
+const (
+	texGopherR = iota
+)
+
+func loadTextures() []sprite.SubTex {
+	a, err := asset.Open("waza-gophers.jpeg")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer a.Close()
+
+	img, _, err := image.Decode(a)
+	if err != nil {
+		log.Fatal(err)
+	}
+	t, err := eng.LoadTexture(img)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return []sprite.SubTex{
+		texGopherR: sprite.SubTex{t, image.Rect(152, 10, 152+140, 10+90)},
+	}
+}
+
+type arrangerFunc func(e sprite.Engine, n *sprite.Node, t clock.Time)
+
+func (a arrangerFunc) Arrange(e sprite.Engine, n *sprite.Node, t clock.Time) { a(e, n, t) }
